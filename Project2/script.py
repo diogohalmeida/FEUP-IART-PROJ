25640ad1edeadbf37e690d8c1532419fe5a5c58f
Diogo Henrique Pinto de Almeida <up201806630@fe.up.pt>
2021-05-25 00:54:39 +0100

    Continued notebook, trying oversampling/undersampling

--- Project2/script.py
+++ Project2/script.py
@@ -37,13 +37,47 @@ clean_data_test = variation_data.drop(columns=["Company"])
 #full_data = pd.merge(ten_k_fillings_data, variation_data, on='Company', how='inner')
 
 
+# Create correlation matrix
+corr_matrix = clean_data.corr().abs()
+
+# Select upper triangle of correlation matrix
+upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
+
+# Find features with correlation greater than 0.95
+to_drop = [column for column in upper.columns if any(upper[column] > 0.95)]
+
+# Drop features 
+clean_data.drop(to_drop, axis=1, inplace=True)
+
+
+down_price_data = clean_data.loc[clean_data['class'] == 0]
+up_price_data = clean_data.loc[clean_data['class'] == 1]
+
+os_dataset = pd.concat([down_price_data.sample(n=451, random_state=1, replace=True), up_price_data])
+us_dataset = pd.concat([down_price_data, up_price_data.sample(n=187, random_state=1)])
+
+
 
 train_split, test_split = train_test_split(clean_data, random_state=1, test_size=0.25, stratify=clean_data['class'])
+
+train_split_os, test_split_os = train_test_split(os_dataset, random_state=1, test_size=0.25, stratify=os_dataset['class'])
+
+train_split_us, test_split_us = train_test_split(us_dataset, random_state=1, test_size=0.25, stratify=us_dataset['class'])
 
 X_train = train_split.iloc[:, :-1].values
 y_train = train_split.iloc[:, -1].values
 X_test = test_split.iloc[:, :-1].values
 y_test = test_split.iloc[:, -1].values
+
+X_train_os = train_split_os.iloc[:, :-1].values
+y_train_os = train_split_os.iloc[:, -1].values
+X_test_os = test_split_os.iloc[:, :-1].values
+y_test_os = test_split_os.iloc[:, -1].values
+
+X_train_us = train_split_us.iloc[:, :-1].values
+y_train_us = train_split_us.iloc[:, -1].values
+X_test_us = test_split_us.iloc[:, :-1].values
+y_test_us = test_split_us.iloc[:, -1].values
 
 
 scaler = StandardScaler()
@@ -51,6 +85,11 @@ scaler.fit(X_train)
 
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.fit_transform(X_test)
+X_train_os = scaler.fit_transform(X_train_os)
+X_test_os = scaler.fit_transform(X_test_os)
+X_train_us = scaler.fit_transform(X_train_us)
+X_test_us = scaler.fit_transform(X_test_us)
+
 
 #sb.pairplot(full_data, hue='class')
 
@@ -136,46 +175,47 @@ X_test = scaler.fit_transform(X_test)
 
 
 #DECISION TREES;
-# decision_tree_classifier = DecisionTreeClassifier(random_state=1)
+decision_tree_classifier = DecisionTreeClassifier(random_state=0)
 
-# parameter_grid = {'criterion': ['gini','entropy'],
-#                   'splitter': ['best', 'random'],
-#                   'max_depth': [1, 2, 3, 4, 5],
-#                   'max_features': [1, 2, 3, 4]}
+parameter_grid = {'criterion': ['gini','entropy'],
+                  'splitter': ['best', 'random'],
+                  'max_depth': [1, 2, 3, 4, 5],
+                  'max_features': [1, 2, 3, 4, 'sqrt', 'auto','log2']}
 
-# grid_search = GridSearchCV(decision_tree_classifier,
-#                             param_grid=parameter_grid,
-#                             scoring='precision_weighted',
-#                             cv=10)
+grid_search = GridSearchCV(decision_tree_classifier,
+                            param_grid=parameter_grid,
+                            scoring='precision_weighted',
+                            cv=10)
 
-# grid_search.fit(X_train, y_train)
-# print('Best score: {}'.format(grid_search.best_score_))
-# print('Best parameters: {}'.format(grid_search.best_params_))
+grid_search.fit(X_train_os, y_train_os)
+print('Best score: {}'.format(grid_search.best_score_))
+print('Best parameters: {}'.format(grid_search.best_params_))
 
 
-# decision_tree_classifier = grid_search.best_estimator_
+decision_tree_classifier = grid_search.best_estimator_
 
-# with open('iris_dtc.dot', 'w') as out_file:
-#     out_file = tree.export_graphviz(decision_tree_classifier, out_file=out_file)
+with open('iris_dtc.dot', 'w') as out_file:
+    out_file = tree.export_graphviz(decision_tree_classifier, out_file=out_file)
    
-# predictions_train = grid_search.predict(X_train)
-# predictions_test = grid_search.predict(X_test) 
+predictions_train = grid_search.predict(X_train)
+predictions_test = grid_search.predict(X_test) 
 
-# print(accuracy_score(y_train, predictions_train))
-# print(accuracy_score(y_test, predictions_test))
+print(accuracy_score(y_train, predictions_train))
 
-# print(classification_report(y_train, predictions_train, target_names=['IGNORE', 'BUY']))
-# print(classification_report(y_test, predictions_test, target_names=['IGNORE', 'BUY']))
+print(accuracy_score(y_test, predictions_test))
+
+print(classification_report(y_train, predictions_train, target_names=['IGNORE', 'BUY']))
+print(classification_report(y_test, predictions_test, target_names=['IGNORE', 'BUY']))
     
     
 #SVM  
-# svm_classifier = SVC(random_state=1)
+# svm_classifier = SVC(random_state=0)
 
 
 # tuned_parameters = [{'kernel': ['rbf', 'linear','poly','sigmoid'], 
 #                       'gamma': ['auto','scale', 1e-3, 1e-4], 
 #                       'C': [0.01, 0.1, 1, 10, 100],
-#                       'class_weight': ['balanced', None]}]
+#                       'tol':[1e-4, 1e-3]}]
 
 
 # grid_search = GridSearchCV(svm_classifier,
@@ -199,14 +239,15 @@ X_test = scaler.fit_transform(X_test)
 
 
 #MLP
-# mlp_classifier = MLPClassifier(random_state=1, early_stopping=False)
+# mlp_classifier = MLPClassifier(random_state=0, early_stopping=False)
 
 
 # tuned_parameters = {'hidden_layer_sizes': [(32,), (64,), (32, 64, 32)],
 #                     'activation': ['logistic','tanh', 'relu'],
-#                     'solver': ['adam', 'sgd', 'lbfgs'], #'lbfgs'
-#                     'alpha': [0.0001, 0.05],
-#                     'learning_rate': ['constant','adaptive']}
+#                     'solver': ['adam', 'sgd', 'lbfgs'],
+#                     'alpha': [0.0001, 0.001, 0.01],
+#                     'learning_rate': ['constant','adaptive'],
+#                     'power_t': [0.25, 0.5, 0.75]}
 
 # grid_search = GridSearchCV(mlp_classifier, 
 #                     tuned_parameters,
